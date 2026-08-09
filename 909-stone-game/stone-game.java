be79186ed18f3a17677fcfc29a246d3a1d5b49e4
Shobhit Kesarwani <sobhitkesrwani2016@gmail.com>
2026-08-09 18:59:41 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (41.77%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,9 +1,5 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        int size=piles.length;
-        int sum = Arrays.stream(piles).sum();
-        int a=(piles[0]+piles[1])-sum;
-        int b=(piles[size-1]+piles[size-2])-sum;
         return true;
     }
 }
